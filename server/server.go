b796694fd9e00d1f0b219d5a3d286d625ff0cb1f
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-07-30 11:27:48 +0530

    Initial Commit

---
+++ server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/DeathStroke19891/transactions/transactions"
+)
+
+var (
+	port       = flag.Int("port", 50051, "The server port")
+)
+
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
